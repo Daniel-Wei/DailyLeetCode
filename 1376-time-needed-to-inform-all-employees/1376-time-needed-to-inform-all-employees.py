3e590d7ee9623bc273be88ee02dc6ec218b97193
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2022-01-21 15:10:10 +1100

    Time: 2496 ms (13.29%), Space: 47.5 MB (43.23%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
@@ -15,32 +15,22 @@ class Solution:
         for i in range(n):
             if i != headID:
                 tree[manager[i]] += [i]
-                
-        
-        #print(tree)
         
         self.end = []
-        
         
         def dfs(nodes, acc):
             if len(nodes) == 0:
                 self.end += [acc]
-                #print(self.end)
-                
+                   
             curr = acc
             for node in nodes:
-                #print(node)
+                
                 next = acc + informTime[node]
                
                 dfs(tree[node], next)
                 
-                
-                
-                
         dfs(tree[headID], 0)
-        #print(tree)
-        #print(self.end)
-        
+       
         return informTime[headID] + max(self.end)
                 
             
