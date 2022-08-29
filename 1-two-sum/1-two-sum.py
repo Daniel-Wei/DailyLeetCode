1614fa992d0b03e27aebbca5f6fcae1ba1e56c1b
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2022-08-29 18:56:24 +1000

    Time: 95 ms (67.85%), Space: 15.2 MB (49.80%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -2,12 +2,15 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hashmap = {}
         
-        
-        
         for index, val in enumerate(nums):
             desired = target - val
+            
+            # 如果所求数已经存在在hashmap中
+            # return 两个indices 因为exactly one solution
             if desired in hashmap:
                 return [hashmap[desired], index]
+            
+            # 将当前数值和index存到hashmap中
             else:
                 hashmap[val] = index
         
