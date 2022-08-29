e7cc52bc132785485e30817bdd3de85112a8aa73
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2022-08-29 18:50:51 +1000

    Time: 40 ms (95.06%), Space: 13.9 MB (18.62%) - LeetHub

--- 763-partition-labels/763-partition-labels.py
+++ 763-partition-labels/763-partition-labels.py
@@ -2,19 +2,86 @@ class Solution:
     def partitionLabels(self, s: str) -> List[int]:
         last = {}
         res = []
-        for i, letter in enumerate(s):
-            last[letter] = i
-            
-        left, right = 0, 0
         
-        for i, letter in enumerate(s):
-            right = max(right, last[letter])
+        # hashtable[char] = char的最右index
+        for i, char in enumerate(s):
+            last[char] = i
             
-            if i == right:
-                res += [right - left + 1]
-                left = right + 1
+        l, r = 0, 0
+        
+        for i, char in enumerate(s):
+            
+            # 持续更新为所有遍历过的char的最右出现位置
+            r = max(r, last[char])
+            
+            # 当前位置恰好是所有遍历过的char的最右出现位置
+            if i == r:
+                res += [r - l + 1] # 进行切割
+                l = r + 1 # 重新准备下一次切割
                 
         return res
+                
+                
+                
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# last = {}
+#         res = []
+#         for i, letter in enumerate(s):
+#             last[letter] = i
+            
+#         left, right = 0, 0
+        
+#         for i, letter in enumerate(s):
+#             right = max(right, last[letter])
+            
+#             if i == right:
+#                 res += [right - left + 1]
+#                 left = right + 1
+                
+#         return res
             
         
             
