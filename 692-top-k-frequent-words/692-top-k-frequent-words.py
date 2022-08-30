0bbdcbeb800e295bfc8a522ae654b3872ca5e5fb
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2022-08-30 16:35:47 +1000

    Time: 75 ms (71.71%), Space: 14.2 MB (5.25%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.py
+++ 692-top-k-frequent-words/692-top-k-frequent-words.py
@@ -1,24 +1,60 @@
+
+
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         freq = {}
-        freqlst = []
+        
         for word in words:
             if word in freq:
                 freq[word] += 1
             else:
                 freq[word] = 1
-        
-        for word in set(words):
-            freqlst += [[word, freq[word]]]
-        
-        sortedlst = sorted(freqlst, key = lambda x: x[0])
-        sortedlst = sorted(sortedlst, key = lambda x: x[1], reverse = True)
-        res = []
-        print(sortedlst)
-        
-        for i in range(k):
-            res += [sortedlst[i][0]]
+                
+# 不能使用heapq 的 nlargest， 其他相同freq但是lexicograpgical order更小的可能被忽略  
+              
+#          for word in set(words):
+#             heapq.heappush(h, (freq[word], word))
             
-        return res
+#         res = heapq.nlargest(k, h)
+#         res = sorted(res, key = lambda x : x[1])
+#         res = sorted(res, key = lambda x : x[0], reverse = True) 
+        
+#         print(res)
+
+
+        res = []
+
+        for word in set(words):
+            res += [[word, freq[word]]]
+            
+        res = sorted(res, key = lambda x : x[0])
+        print(res)
+        res = sorted(res, key = lambda x: x[1], reverse = True)
+        
+        print(res)
+        return [item[0] for item in res[:k]]
+            
+        
+        
+#         freq = {}
+#         freqlst = []
+#         for word in words:
+#             if word in freq:
+#                 freq[word] += 1
+#             else:
+#                 freq[word] = 1
+        
+#         for word in set(words):
+#             freqlst += [[word, freq[word]]]
+        
+#         sortedlst = sorted(freqlst, key = lambda x: x[0])
+#         sortedlst = sorted(sortedlst, key = lambda x: x[1], reverse = True)
+#         res = []
+#         print(sortedlst)
+        
+#         for i in range(k):
+#             res += [sortedlst[i][0]]
+            
+#         return res
             
             
