9ac45d69016deedf14e0cd634d74b06cb6693870
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2022-11-15 19:17:30 +1100

    Time: 77 ms (33.87%), Space: 115.1 MB (70.21%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,15 +1,12 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        Arrays.sort(points, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                int p1 = (int) Math.pow(o1[0], 2) + (int) Math.pow(o1[1], 2);
-                int p2 = (int) Math.pow(o2[0], 2) + (int) Math.pow(o2[1], 2);
-                if(p1 >= p2){
-                    return 1;
-                }else{
-                    return -1;
-                }
+        Arrays.sort(points, (o1, o2) -> {
+            int p1 = (int) Math.pow(o1[0], 2) + (int) Math.pow(o1[1], 2);
+            int p2 = (int) Math.pow(o2[0], 2) + (int) Math.pow(o2[1], 2);
+            if(p1 >= p2){
+                return 1;
+            }else{
+                return -1;
             }
         });
 
