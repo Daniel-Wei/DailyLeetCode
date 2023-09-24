a3b234168edfe98860a811235847510b384316c1
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2023-09-24 23:24:53 +1000

    Time: 152 ms (20.68%), Space: 95.1 MB (5.11%) - LeetHub

--- 0322-coin-change/0322-coin-change.cs
+++ 0322-coin-change/0322-coin-change.cs
@@ -1,7 +1,5 @@
 public class Solution {
     public int CoinChange(int[] coins, int amount) {
-        
-        
         int[]dpMemo = new int[amount + 1];
         
         for(int i = 1; i< dpMemo.Length; i++){
@@ -17,8 +15,6 @@ public class Solution {
         
         dpMemo[0] = 0;
         
-       
-        
         for(int i = 1; i <= amount; i++){
             Array.ForEach(coins, c => {
                 if(c <= i){
@@ -33,13 +29,6 @@ public class Solution {
                 
                 
             });
-            
-//             Array.ForEach(dpMemo, m => {
-//                     Console.Write(m);
-//                     Console.Write(" ");
-//                 });
-//                 Console.WriteLine();
-            
           
         }
         
