38c3e7d6fe402ae65c9ac7ce2ae5c920b768b788
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2023-09-27 01:48:13 +1000

    Time: 92 ms (84.42%), Space: 48.7 MB (20.78%) - LeetHub

--- 0494-target-sum/0494-target-sum.cs
+++ 0494-target-sum/0494-target-sum.cs
@@ -1,17 +1,40 @@
 public class Solution {
     public int FindTargetSumWays(int[] nums, int target) {
-        List<int> possibleSums = new List<int>(){nums[0], -nums[0]};
-        for(int i = 1; i < nums.Length; i++){
-            List<int> curr = new();
-            possibleSums.ForEach(sum => {
-                curr.Add(sum + nums[i]);
-                curr.Add(sum - nums[i]);
-            });
-            possibleSums = curr; 
-            
-           
+        
+        // Basic Solution
+//         List<int> possibleSums = new List<int>(){nums[0], -nums[0]};
+//         for(int i = 1; i < nums.Length; i++){
+//             List<int> curr = new();
+//             possibleSums.ForEach(sum => {
+//                 curr.Add(sum + nums[i]);
+//                 curr.Add(sum - nums[i]);
+//             });
+//             possibleSums = curr; 
+//         }
+        
+//         return possibleSums.Where(s => s == target).Count();
+        
+        
+        int sum = nums.Sum();
+        //edge cases
+        if(target > sum || target < -sum){
+            return 0;
         }
         
-        return possibleSums.Where(s => s == target).Count();
+        //1-d memo dp
+        //no explicitly saving all possible sums every time
+        int[]memo = new int[sum * 2 + 1];
+        memo[sum] = 1; //represents 0
+        for(int i = 0; i < nums.Length; i++){
+            int[]next = new int[sum * 2 + 1];
+            for(int k = 0; k < sum * 2 + 1; k++){
+                if(memo[k] != 0){
+                    next[k + nums[i]] += memo[k];
+                    next[k - nums[i]] += memo[k];
+                }
+            }
+            memo = next;
+        }
+        return memo[sum + target];
     }
 }
