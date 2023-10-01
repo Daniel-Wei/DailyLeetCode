de03274995010e3c6658a221518e6152b291c611
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2023-10-02 01:02:16 +1100

    Time: 88 ms (62.87%), Space: 39.9 MB (72.46%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cs
+++ 0486-predict-the-winner/0486-predict-the-winner.cs
@@ -1,37 +1,39 @@
 public class Solution {
     public bool PredictTheWinner(int[] nums) {
         int n = nums.Length;
-    if (n <= 2)
-        return true;
-    
-    int[][] dp = new int[n][];
-    for (int i =0 ; i < n; i++)
-    {
-        dp[i] = new int[n];
-    }
-    int sum =0;
-    for (int i =0 ; i < n;i++)
-    {
-        dp[i][i] = nums[i];
-        sum += nums[i];
-    }
-    
-    for (int i =1 ; i <= n-1;i++)
-    {
-        for (int j = 0 ; j < n-i;j++)//check this
-        {
-            int x =0,y = 0;
-            if (i != 1)
-            {
-                x = Math.Min(dp[j+1][i+j-1], dp[j+2][i+j]);
-                y = Math.Min(dp[j][i+j-2], dp[j+1][i+j-1]);  
+        int totalSum = nums.Sum();
+        int start = 0;
+        int end = n - 1;
+        
+        int[ , ]dp = new int[n, n];
+        
+        
+        for(int i = 0; i < n; i++){
+            int currSum = nums[i];
+            for(int j = i; j >= 0; j--){
+                
+                if(i == j){
+                    dp[i, i] = nums[i];
+                    
+                }else{
+                    currSum += nums[j];
+                    dp[j, i] = Math.Max(currSum - dp[j + 1, i], currSum - dp[j, i - 1]);
+                }
+                
+                
+                
             }
-
-            dp[j][i+j] = Math.Max(nums[j] + x, nums[i+j] + y ); 
         }
+        
+     
+      
+        
+        return 2 * dp[0, n - 1] >= totalSum;
+        
+       
+        
+    
     }
-    int p1 =  dp[0][n-1];
-    int p2 = sum-p1;
-    return p1 >= p2 ? true : false;
-    }
+    
+   
 }
