2c30027e3aecaed9130bbb09dbe67b1caa84b3d1
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2023-10-02 01:26:18 +1100

    Time: 82 ms (85.63%), Space: 40 MB (55.09%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cs
+++ 0486-predict-the-winner/0486-predict-the-winner.cs
@@ -2,16 +2,11 @@ public class Solution {
     public bool PredictTheWinner(int[] nums) {
         int n = nums.Length;
         int totalSum = nums.Sum();
-        int start = 0;
-        int end = n - 1;
-        
         int[ , ]dp = new int[n, n];
-        
         
         for(int i = 0; i < n; i++){
             int currSum = nums[i];
             for(int j = i; j >= 0; j--){
-                
                 if(i == j){
                     dp[i, i] = nums[i];
                     
@@ -19,21 +14,9 @@ public class Solution {
                     currSum += nums[j];
                     dp[j, i] = Math.Max(currSum - dp[j + 1, i], currSum - dp[j, i - 1]);
                 }
-                
-                
-                
             }
         }
         
-     
-      
-        
         return 2 * dp[0, n - 1] >= totalSum;
-        
-       
-        
-    
     }
-    
-   
 }
