7b54246c069a6d6f22848e37fc821733399a9b5a
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2023-10-03 03:48:32 +1100

    Time: 2831 ms (6.67%), Space: 207.8 MB (6.67%) - LeetHub

---
+++ 0464-can-i-win/0464-can-i-win.cs
@@ -0,0 +1,46 @@
+public class Solution {
+    Dictionary<string, bool> cache = new Dictionary<string, bool>();
+    
+    public bool CanIWin(int maxChoosableInteger, int desiredTotal) {
+        if(maxChoosableInteger >= desiredTotal){
+            return true;
+        }
+        
+        int[]used = new int[maxChoosableInteger + 1];
+        
+        
+        if((1 + maxChoosableInteger) * maxChoosableInteger / 2 < desiredTotal ){
+            return false;
+        }
+        
+        return canIWinWithSituation(maxChoosableInteger, desiredTotal, used);
+    }
+    
+   private bool canIWinWithSituation(int maxChoosableInteger, int curDesiredTotal, int[] chosen) {
+        
+        if (curDesiredTotal <= 0) { 
+            return false;
+        }
+        
+        String chosenSerialization = string.Join("",chosen);
+       
+        if (cache.ContainsKey(chosenSerialization)) {
+            return cache[chosenSerialization];
+        }
+        
+        for (int i = 1; i <= maxChoosableInteger; i++) {
+            if (chosen[i] == 1) {
+                continue;
+            }
+            chosen[i] = 1;
+            if (!canIWinWithSituation(maxChoosableInteger, curDesiredTotal - i, chosen)) {
+                cache[chosenSerialization]= true;
+                chosen[i] = 0;
+                return true;
+            }
+            chosen[i] = 0;
+        }
+        cache[chosenSerialization] = false;
+        return false;
+    }
+}
