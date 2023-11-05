ebb7103b3a17897a42cf54e17e66d6325bb9a9af
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2023-11-06 00:07:42 +1100

    Time: 81 ms (79.17%), Space: 39.6 MB (62.50%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cs
+++ 1140-stone-game-ii/1140-stone-game-ii.cs
@@ -1,44 +1,43 @@
 public class Solution {
+    private int n;
+    private int[ , ] dp;
+    private int[] sums;
+    private int[] piles;
+    
     public int StoneGameII(int[] piles) {
-        int n = piles.Length;
-        int m = n / 2;
-        int[, ] dp = new int[n, n];
-        
-        int[] sums = new int[n];
+        this.n = piles.Length;
+        this.dp = new int[n, n];
+        this.sums = new int[n];
+        this.piles = piles;
         
         sums[n-1] = piles[n-1];
         for(int i = n-2; i >= 0; i--){
             sums[i] = sums[i + 1] + piles[i];
         }
         
-        return helper(0, 1, dp, piles, sums);
+        return helper(0, 1);
     }
     
-    public int helper(int i, int m, int[,] dp, int[]piles, int[]sums){
-        if(i == piles.Length){
-            return 0;
+    public int helper(int i, int m){
+        
+        
+        if(i + m * 2 >= n){
+            return this.sums[i];
         }
         
-        if(i + m * 2 >= piles.Length){
-            return sums[i];
-        }
-        
-        if(dp[i, m] != 0){
-            return dp[i, m];
+        if(this.dp[i, m] != 0){
+            return this.dp[i, m];
         }
         
         int min = int.MaxValue;
         
         for(int x = 1; x <= 2 * m; x++){
-            min = Math.Min(min, helper(i+x, Math.Max(x, m), dp, piles, sums));
+            if(i + x < n){
+                 min = Math.Min(min, helper(i+x, Math.Max(x, m)));
+            }
         }
         
-        dp[i, m] = sums[i] - min;
-        return dp[i, m];
-            
-            
-        
+        this.dp[i, m] = this.sums[i] - min;
+        return this.dp[i, m];
     }
-    
-    
 }
