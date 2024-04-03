1fcfe3226e0ad59d875eed9de290d1833b748a49
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-04-04 00:50:39 +1100

    Time: 265 ms (67.63%), Space: 42.5 MB (38.86%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cs
@@ -0,0 +1,54 @@
+public class Solution {
+    public bool Exist(char[][] board, string word) {
+        int m = board.Length;
+        int n = board[0].Length;
+        bool result = false;
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(word[0] == board[i][j]){
+                    result = result || BackTrack(board, 0, i, j, m, n, word);
+                }
+            }
+        }
+        
+        return result;
+    }
+    
+    private bool BackTrack(char[][]board, int index, int row, int col, int m, int n, string word){
+        
+        //base case
+        if(index == word.Length){
+            return true;
+        }
+        
+        if(row < 0 || row == m || col < 0 || col == n || board[row][col] == '#'){
+            return false;
+        }
+        
+        // Console.WriteLine(row);
+        // Console.WriteLine(col);
+        // Console.WriteLine(index);
+        // Console.WriteLine();
+
+        char curr = board[row][col];
+        
+        board[row][col] = '#';
+        
+        if(word[index] != curr){
+            board[row][col] = curr;
+            
+            return false;
+        }else{
+            
+                bool res =  BackTrack(board, index + 1, row + 1, col, m, n, word) || BackTrack(board, index + 1, row - 1, col, m, n, word)
+                    || BackTrack(board, index + 1, row, col + 1, m, n, word) || BackTrack(board, index + 1, row, col - 1, m, n, word);
+            if(!res){
+                board[row][col] = curr;
+            }
+            
+            return res;
+            
+        }
+    }
+}
