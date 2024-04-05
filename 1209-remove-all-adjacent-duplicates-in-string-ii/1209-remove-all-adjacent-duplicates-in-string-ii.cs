54386fb2d3694fa4f7672e1bbb0f3902fc2e033a
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-04-06 02:34:27 +1100

    Time: 64 ms (84.34%), Space: 45.3 MB (46.39%) - LeetHub

---
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cs
@@ -0,0 +1,37 @@
+public class Solution{
+    public string RemoveDuplicates(string s, int k)
+    {
+        //no need to track specific index of each char
+        //just record latest count of each char
+        Stack<(char ch, int count)> stack = new Stack<(char, int)>();
+        StringBuilder sb = new StringBuilder();
+        
+        foreach(char c in s){
+            if(stack.Any() && stack.Peek().ch == c){
+                var temp = stack.Pop();
+                temp.count += 1;
+                
+                if(temp.count != k){
+                    stack.Push(temp);
+                }
+            }else{
+                stack.Push((c, 1));
+            }
+        }
+        
+        
+        while (stack.Any())
+        {
+            var top = stack.Pop();
+            for (int i = 0; i < top.count; i++)
+            {
+                sb.Append(top.ch);
+            }
+        }
+        
+        char[]chars = sb.ToString().ToCharArray();
+        Array.Reverse(chars);
+        //FILO
+        return new string(chars);
+    }
+}
