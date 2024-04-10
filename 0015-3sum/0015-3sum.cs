da0e6c672bd7f9a302bf185a4accc9809b0ba080
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-04-11 02:49:49 +1000

    Time: 353 ms (21.82%), Space: 78.2 MB (22.91%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cs
@@ -0,0 +1,44 @@
+public class Solution {
+    public IList<IList<int>> ThreeSum(int[] nums) {
+        Array.Sort(nums);
+        int l = 0;
+        int r = nums.Length - 1;
+        int curr = 0;
+        int sum = 0;
+        IList<IList<int>> res = new List<IList<int>>();
+        Dictionary<int, HashSet<int>> dict = new Dictionary<int, HashSet<int>>();
+        
+        for(int i = 0; i < nums.Length - 2; i++){
+            l = i + 1;
+            r = nums.Length - 1;
+            curr = nums[i];
+
+            while(l < r){
+                sum = nums[l] + nums[r];
+
+                if(sum > -curr){
+                    r -= 1;
+                }else if(sum == -curr){
+                    
+                    if(!dict.ContainsKey(curr)){
+                        dict.Add(curr, new HashSet<int>{nums[l]});
+                        res.Add(new List<int>{curr, nums[l], nums[r]});
+                    }else{
+                        
+                        if(!dict[curr].Contains(nums[l])){
+                            dict[curr].Add(nums[l]);
+                            res.Add(new List<int>{curr, nums[l], nums[r]});
+                        }
+                    }
+                    l += 1;
+                }else{
+                    l += 1;
+                }
+            }
+            
+           
+        }
+        
+        return res;
+    }
+}
