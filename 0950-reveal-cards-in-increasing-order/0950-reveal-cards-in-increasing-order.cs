4dd57183606a1ddd176e0f3da4e2425bebfb19a4
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-04-11 00:30:41 +1000

    Time: 107 ms (57.89%), Space: 45.9 MB (94.74%) - LeetHub

---
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int[] DeckRevealedIncreasing(int[] deck) {
+        
+        if(deck.Length == 1){
+            return deck;
+        }
+        
+        Queue<int> q= new Queue<int>();
+        Array.Sort(deck, (a, b) => b.CompareTo(a));
+        q.Enqueue(deck[0]);
+        q.Enqueue(deck[1]);
+        
+        for(int i = 2; i < deck.Length; i++){
+            int curr = q.Peek();
+            q.Dequeue();
+            q.Enqueue(curr);
+            q.Enqueue(deck[i]);
+        }
+        
+        int[]res = q.ToArray();
+        Array.Reverse(res);
+        
+        return res;
+    }
+}
