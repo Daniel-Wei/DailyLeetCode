23951b9fe01637f58844fd868a99ff65bbb0df9e
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-04-15 02:21:36 +1000

    Time: 316 ms (34.56%), Space: 52.7 MB (80.13%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cs
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cs
@@ -1,15 +1,18 @@
 public class Solution {
     public int LargestRectangleArea(int[] heights) {
         Stack<int> s = new Stack<int>();
-        int res = 0;
+        int res = 0, h = 0, last = 0, w = 0;
         
         for(int i = 0; i <= heights.Length; i++){
             
-            int h = i == heights.Length ? 0 : heights[i];
+            //Make sure all elements pop out, avoid cases like 
+            h = i == heights.Length ? 0 : heights[i];
             
             while(s.Any() && h < heights[s.Peek()]){
-                int last = s.Pop();
-                int w = s.Any()? i - s.Peek() - 1 : i;
+                last = s.Pop();
+                
+                //if stack is empty: such heights[last] is all over
+                w = s.Any()? i - s.Peek() - 1 : i;
                 res = Math.Max(res, heights[last] * w);
             }
             s.Push(i);
