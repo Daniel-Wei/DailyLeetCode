cb69ed07b64026ebf86958323dccf58f91c9e345
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-04-21 20:16:16 +1000

    Time: 779 ms (41.08%), Space: 149.9 MB (75.76%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cs
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cs
@@ -1,44 +1,72 @@
 public class Solution {
-        public bool ValidPath(int n, int[][] edges, int source, int destination)
-        {
-            var adjacencyList = GetAdjacencyList(edges);
-            bool[] visited = new bool[n];
-            Stack<int> stack = new Stack<int>();
-            stack.Push(source);
-
-            while (stack.Count > 0)
-            {
-                var top = stack.Pop();
-                visited[top] = true;
-
-                if (top == destination)
-                    return true;
-
-                foreach(var neighbour in adjacencyList.GetValueOrDefault(top))
-                {
-                    if(!visited[neighbour])
-                        stack.Push(neighbour);
+    public bool ValidPath(int n, int[][] edges, int source, int destination) {
+        HashSet<int>visited = new HashSet<int>();
+        Dictionary<int, List<int>> dict = new Dictionary<int, List<int>>();
+        Stack<int>s = new Stack<int>();
+        bool res = false;
+        
+        foreach(int[]edge in edges){
+            if(!dict.ContainsKey(edge[0])){
+                dict.Add(edge[0], new List<int>{edge[1]});
+            }else{
+                dict[edge[0]].Add(edge[1]);
+            }
+            
+            if(!dict.ContainsKey(edge[1])){
+                dict.Add(edge[1], new List<int>{edge[0]});
+            }else{
+                dict[edge[1]].Add(edge[0]);
+            }
+        }
+        
+        s.Push(source);
+        
+        while(s.Any()){
+            int curr = s.Pop();
+            visited.Add(curr);
+            
+            if(curr == destination){
+                res = true;
+                break;
+            }
+            
+            if(dict.ContainsKey(curr)){
+                foreach(int node in dict[curr]){
+                    if(!visited.Contains(node)){
+                        s.Push(node);
+                    }
                 }
             }
-            return false;
         }
+        
+        return res;
+        
+        // return Helper(dict, visited, source, destination);
+    }
+    
+    // Stack Overflow
+//     private bool Helper(Dictionary<int, List<int>>dict, HashSet<int>visited, int curr, int destination){
+//         if(curr == destination){
+//             return true;
+//         }
+        
+//         bool res = false;
+        
+//         if(dict.ContainsKey(curr)){
+//             visited.Add(curr);
+//             List<int>next = dict[curr];
+            
+//             foreach(int node in next){
+//                 if(!visited.Contains(node)){
+//                     res = res || Helper(dict, visited, node, destination);
+//                     if(res){
+//                         break;
+//                     }
+//                 }
+//             }
+            
+//         }
 
-        private Dictionary<int, List<int>> GetAdjacencyList(int[][] edges)
-        {
-            var adjacencyList = new Dictionary<int, List<int>>();
-
-            for (int v = 0; v < edges.Length; v++)
-            {
-                if (adjacencyList.ContainsKey(edges[v][0]))
-                    adjacencyList.GetValueOrDefault(edges[v][0]).Add(edges[v][1]);
-                else
-                    adjacencyList[edges[v][0]] = new List<int>() { edges[v][1] };
-
-                if (adjacencyList.ContainsKey(edges[v][1]))
-                    adjacencyList.GetValueOrDefault(edges[v][1]).Add(edges[v][0]);
-                else
-                    adjacencyList[edges[v][1]] = new List<int>() { edges[v][0] };
-            }
-            return adjacencyList;
-        }
+//         return res;
+//     }
 }
