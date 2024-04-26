4522b577edef0578fca039383f4bf168ad60fd99
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-04-26 16:33:22 +1000

    Time: 223 ms (25.00%), Space: 51.6 MB (25.00%) - LeetHub

---
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cs
@@ -0,0 +1,36 @@
+public class Solution {
+    public int MinFallingPathSum(int[][] grid) {
+        int n = grid.Length;
+        int[,]dp = new int[n, n];
+        
+        for(int i = 0; i < n; i++){
+            dp[0,i] = grid[0][i];
+        }
+        
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < n; j++){
+                
+                dp[i, j] = int.MaxValue;
+                
+                for(int k = 0; k < n; k++){
+                    if(k != j){
+                        dp[i, j] = Math.Min(dp[i, j], dp[i-1, k]);
+                    }
+                }
+                
+                dp[i,j] += grid[i][j];
+                
+                
+            }
+        }
+        
+        int res = int.MaxValue;
+        
+        for(int i = 0; i < n; i++){
+            // Console.WriteLine(dp[n-1, i]);
+            res = Math.Min(res, dp[n-1,i]);
+        }
+        
+        return res;
+    }
+}
