e203fbdbee780c9ec44648fa980fcb0b035e888e
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-04-27 15:25:34 +1000

    Time: 85 ms (100.00%), Space: 42.9 MB (100.00%) - LeetHub

--- 0514-freedom-trail/0514-freedom-trail.cs
+++ 0514-freedom-trail/0514-freedom-trail.cs
@@ -1,22 +1,25 @@
-class Solution {
-    public int findRotateSteps(String ring, String key) {
-        int n = ring.length();
-        int m = key.length();
-        int[][] dp = new int[m + 1][n];
+public class Solution {
+    public int FindRotateSteps(string ring, string key) {
+        int n = ring.Length;
+        int m = key.Length;
+        int[,]dp = new int[m+1, n];
         
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = 0; j < n; j++) {
-                dp[i][j] = Integer.MAX_VALUE;
-                for (int k = 0; k < n; k++) {
-                    if (ring.charAt(k) == key.charAt(i)) {
-                        int diff = Math.abs(j - k);
-                        int step = Math.min(diff, n - diff);
-                        dp[i][j] = Math.min(dp[i][j], step + dp[i + 1][k]);
+        for(int i = m-1; i >=0; i--){
+            for(int j =0; j < n; j++){
+                dp[i,j] = int.MaxValue;
+                
+                for(int k = 0; k < n; k++){
+                    if(ring[k] == key[i]){
+                        int diff = Math.Abs(j - k);
+                        int step = Math.Min(diff, n - diff);
+                        dp[i,j] = Math.Min(dp[i, j], dp[i+1, k] + step);
                     }
                 }
             }
         }
         
-        return dp[0][0] + m;
+        
+        
+        return dp[0,0] + m;
     }
 }
