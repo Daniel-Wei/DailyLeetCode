fd71b4c536527afe14288d5050e1f4bf17be34f7
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-04-27 15:31:51 +1000

    Time: 75 ms (100.00%), Space: 42.3 MB (100.00%) - LeetHub

--- 0514-freedom-trail/0514-freedom-trail.cs
+++ 0514-freedom-trail/0514-freedom-trail.cs
@@ -4,6 +4,11 @@ public class Solution {
         int m = key.Length;
         int[,]dp = new int[m+1, n];
         
+        //对于ith的key的char，以及当前转盘停在jth
+        //转至kth的转盘，和ith的key的char相同
+        //需要走的步数就是Math.Abs(j-k) = step, 或者反方向 Math.Min(step, n-step)
+        //dp[i+1][k]就是key的下一个char 以及转盘停在kth
+        //dp[i+1][k] + step就是（下一个char + 转盘停在kth） + 转到（ith char in key + 转盘在jth）
         for(int i = m-1; i >=0; i--){
             for(int j =0; j < n; j++){
                 dp[i,j] = int.MaxValue;
@@ -17,8 +22,6 @@ public class Solution {
                 }
             }
         }
-        
-        
         
         return dp[0,0] + m;
     }
