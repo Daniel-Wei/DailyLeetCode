1a1814889d61152a83dc47b1100f43528aacd35b
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-06-10 04:33:37 +1000

    Time: 821 ms (6.95%), Space: 53.1 MB (37.24%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.cs
+++ 0846-hand-of-straights/0846-hand-of-straights.cs
@@ -1,19 +1,23 @@
 public class Solution {
     public bool IsNStraightHand(int[] hand, int groupSize) {
          
-        var dict = new Dictionary<int, int>();
-        foreach(var num in hand){
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        
+        foreach(int num in hand){
             if(!dict.ContainsKey(num))
                 dict.Add(num, 0);
             dict[num]++;
         }
+
         while(dict.Count >= groupSize){
-            var min = dict.Keys.Min();
-            for(var i = 0; i < groupSize; i++){
+            int min = dict.Keys.Min();
+            for(int i = 0; i < groupSize; i++){
                 if(dict.ContainsKey(min)){
                     dict[min]--;
+                    
                     if(dict[min] == 0)
                         dict.Remove(min);
+                    
                     min++;
                 }
                 else{
