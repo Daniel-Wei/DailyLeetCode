13d05f1b5d2828c8c7c3a177fa377c6c1a3b215b
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-06-14 20:06:33 +1000

    Time: 340 ms (62.50%), Space: 58.6 MB (12.50%) - LeetHub

---
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int MinIncrementForUnique(int[] nums) {
+        HashSet<int> hs = new HashSet<int>();
+        Array.Sort(nums);
+        int res = 0;
+        int prev = nums[0];
+        
+        for(int i = 1; i < nums.Length; i++){
+            int curr = nums[i];
+            
+            if(curr <= prev){
+                res += prev - curr + 1;
+                prev++;
+            }else{
+                prev = curr;
+            }
+            
+        }
+        
+        return res;
+    }
+}
