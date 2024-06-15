88ad54f9d24290fb7b98e21752b1bc06340a1ae1
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-06-16 01:53:28 +1000

    Time: 500 ms (29.41%), Space: 75.6 MB (25.00%) - LeetHub

---
+++ 0502-ipo/0502-ipo.cs
@@ -0,0 +1,34 @@
+public class Solution {
+    public int FindMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        PriorityQueue<int[], int> pqCapital = new PriorityQueue<int[], int>();
+        PriorityQueue<int, int> pqProfit = new PriorityQueue<int, int>();
+        
+        
+        for(int i = 0; i < profits.Length; i++){
+            // order by capital asc
+            pqCapital.Enqueue(new int[]{capital[i], profits[i]}, capital[i]);
+        }
+        
+        while(k > 0){
+            while(pqCapital.Count > 0 && pqCapital.Peek()[0] <= w){
+                int[]curr = pqCapital.Dequeue();
+                 
+                // order by profit desc
+                // and it would not lose any data
+                // example: 3, 0, [1, 2, 3], [0, 1, 1]
+                // if just keep dequeue() pgCapital, profit 2- capital 1 will be lost
+                pqProfit.Enqueue(curr[1], -curr[1]);
+            }
+            
+            if(pqProfit.Count != 0){
+                w += pqProfit.Dequeue();
+            }else{
+                break;
+            }
+            
+            k--;
+        }
+        
+        return w;
+    }
+}
