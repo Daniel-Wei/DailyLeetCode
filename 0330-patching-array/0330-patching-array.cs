fe5a5b1a1166954985aa0e69883285a7f17e4948
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-06-16 23:33:10 +1000

    Time: 73 ms (75.00%), Space: 42 MB (37.50%) - LeetHub

---
+++ 0330-patching-array/0330-patching-array.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int MinPatches(int[] nums, int n) {
+        Array.Sort(nums);
+        
+        int i = 0;
+        long currMax = 0;
+        int res = 0;
+        
+        while(currMax < n){
+            
+            if(i < nums.Length && nums[i] <= currMax + 1){
+                currMax += nums[i];
+                i++;
+            }else{
+                currMax += currMax + 1;
+                res++;
+            }
+        }
+        
+        return res;
+    }
+}
