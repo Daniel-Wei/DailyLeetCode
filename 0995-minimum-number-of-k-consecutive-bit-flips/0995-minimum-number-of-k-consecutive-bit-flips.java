8ae1f2a20694b4497eb2d7478564e6fc743ce1af
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-06-25 01:38:24 +1000

    Time: 15 ms (29.03%), Space: 57.4 MB (18.71%) - LeetHub

--- 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.java
+++ 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.java
@@ -1,18 +1,27 @@
 class Solution {
     public int minKBitFlips(int[] nums, int k) {
-        int n = nums.length, flipped = 0, res = 0;
-        int[] isFlipped = new int[n];
-        for (int i = 0; i < nums.length; ++i) {
-            if (i >= k)
-                flipped ^= isFlipped[i - k];
-                if (flipped == nums[i]) {
-                    if (i + k > nums.length)
-                        return -1;
-                isFlipped[i] = 1;
-                flipped ^= 1;
+        Deque<Integer> deque = new ArrayDeque<Integer>();
+        int n = nums.length;
+        int res = 0;
+        
+        for(int i = 0; i < n; i++){
+                
+            // keep the sliding window size as k
+            while(deque.size() > 0 && i > deque.peekFirst() + k - 1){
+                deque.pollFirst();
+            }
+            
+            // greedily flip bits whenever there is a 0
+            if((deque.size() + nums[i]) % 2 == 0){
+                if(i + k > n){
+                    return -1;
+                }
+
                 res++;
+                deque.add(i);
             }
         }
+        
         return res;
     }
 }
