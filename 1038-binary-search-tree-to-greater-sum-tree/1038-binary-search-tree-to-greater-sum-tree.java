7376401b2703f7fad18511071e75b99a55cbfccb
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-06-26 00:44:57 +1000

    Time: 0 ms (100.00%), Space: 40.9 MB (73.87%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.java
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.java
@@ -1,14 +1,31 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    val = 0
-    def bstToGst(self, root):
-        if root.right: self.bstToGst(root.right)
-        root.val = self.val = self.val + root.val
-        if root.left: self.bstToGst(root.left)
-        return root
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int val;
+    public TreeNode bstToGst(TreeNode root) {
+        if(root.right != null){
+            bstToGst(root.right);
+        }
         
+        val = root.val = root.val + val;
+        
+        if(root.left != null){
+            bstToGst(root.left);
+        }
+        
+        return root;
+    }
+}
