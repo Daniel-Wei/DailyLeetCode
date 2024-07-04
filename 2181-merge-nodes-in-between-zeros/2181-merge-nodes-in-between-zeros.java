9b9dacf795e72fa55c053a423d763de7839178e7
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-07-05 00:13:55 +1000

    Time: 5 ms (84.77%), Space: 81.7 MB (68.31%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
@@ -10,36 +10,63 @@
  */
 class Solution {
     public ListNode mergeNodes(ListNode head) {
-        ListNode res = new ListNode();
-        ListNode newHead = res;
+//         ListNode res = new ListNode();
+//         ListNode newHead = res;
         
-        int count = 0;
-        int sum = 0;
+//         int count = 0;
+//         int sum = 0;
         
-        while(head != null){
-            if(head.val == 0){
-                if(count == 0){
-                    count ++;
-                }else{
-                    newHead.val = sum;
+//         while(head != null){
+//             if(head.val == 0){
+//                 if(count == 0){
+//                     count ++;
+//                 }else{
+//                     newHead.val = sum;
                     
-                    if(head.next != null){
-                        ListNode next = new ListNode();
-                        newHead.next = next;
-                        newHead = next;
-                        sum = 0;
-                    }
+//                     if(head.next != null){
+//                         ListNode next = new ListNode();
+//                         newHead.next = next;
+//                         newHead = next;
+//                         sum = 0;
+//                     }
+//                 }
+//             }
+            
+//             if(count == 1){
+//                 sum += head.val;
+//             }
+            
+//             head = head.next;
+//         }
+        
+//         return res;
+        
+        head = head.next;
+        ListNode p1 = head;
+        ListNode p2 = head.next;
+        
+        
+        while(p2 != null){
+            
+            
+            
+            if(p2.val != 0){
+                p1.val += p2.val;
+            }else{
+                p1.next = p2.next;
+                p1 = p2.next;
+                
+                if(p1 == null){
+                    break;
+                }else{
+                    p2 = p2.next;
                 }
             }
             
-            if(count == 1){
-                sum += head.val;
-            }
-            
-            head = head.next;
+            p2 = p2.next;
         }
         
-        return res;
+        return head;
         
     }
 }
