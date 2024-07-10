8ab9e5ad9241f073ebe5a9b19d824f5d8b69ac23
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-07-10 22:38:38 +1000

    Time: 3 ms (44.92%), Space: 41.9 MB (24.23%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -4,31 +4,26 @@ class Solution {
         int last = 0;
         int secondLast = 0;
         int next = 0;
-        int asciiDiff = 0;
         char c = '_';
         
         for(String o : operations){
             c = o.charAt(o.length() - 1);
-            asciiDiff = c - '0';
-            
-            if(asciiDiff >= 0 && asciiDiff <= 9){
-                s.push(Integer.parseInt(o));
+            if(c == 'C'){
+                s.pop();
+            }else if(c == '+'){
+                last = s.pop();
+                secondLast = s.pop();
+                next = last + secondLast;
+                s.push(secondLast);
+                s.push(last);
+                s.push(next);
+            }else if(c == 'D'){
+                last = s.pop();
+                next = last * 2;
+                s.push(last);
+                s.push(next);
             }else{
-                if(c == 'C'){
-                    s.pop();
-                }else if(c == '+'){
-                    last = s.pop();
-                    secondLast = s.pop();
-                    next = last + secondLast;
-                    s.push(secondLast);
-                    s.push(last);
-                    s.push(next);
-                }else{
-                    last = s.pop();
-                    next = last * 2;
-                    s.push(last);
-                    s.push(next);
-                }
+                s.push(Integer.parseInt(o));
             }
         }
         
