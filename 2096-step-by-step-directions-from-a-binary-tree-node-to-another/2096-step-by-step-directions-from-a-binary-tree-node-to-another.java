9f139400d61e17127720cb74993536579be51740
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-07-17 00:33:37 +1000

    Time: 546 ms (11.30%), Space: 79.7 MB (72.26%) - LeetHub

---
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public String getDirections(TreeNode root, int startValue, int destValue) {
+        
+        StringBuilder sPath = new StringBuilder();
+        getPathFromRoot(root, startValue, sPath);
+        StringBuilder dPath = new StringBuilder();
+        getPathFromRoot(root, destValue, dPath);
+    
+        int i = 0;
+
+        //remove initial common paths
+        while(i < sPath.length() && i < dPath.length()){
+            if(sPath.charAt(i) == dPath.charAt(i)){
+                i++;
+            }else{
+                break;
+            }
+        }
+        
+        return "U".repeat(sPath.length() - i) + dPath.substring(i);
+    }
+    
+    private boolean getPathFromRoot(TreeNode curr, int nodeVal, StringBuilder sb){
+        if(curr.val != nodeVal){
+            if(curr.left != null && getPathFromRoot(curr.left, nodeVal, sb)){
+                sb.insert(0, 'L');
+                return true;
+            } 
+            
+            if(curr.right != null && getPathFromRoot(curr.right, nodeVal, sb)){
+                sb.insert(0, 'R');
+                return true;
+                
+            }
+            
+            return false;
+        }else{
+            return true;
+        }
+    }
+    
+}
