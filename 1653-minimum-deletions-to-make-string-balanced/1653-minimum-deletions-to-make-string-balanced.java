ea0fc44d769a5a764e910979edb51d0f6246af3f
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-07-31 02:41:55 +1000

    Time: 23 ms (69.92%), Space: 45.5 MB (39.55%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
@@ -5,15 +5,16 @@ class Solution {
         int b = 0;
         
         for(int i = 0; i < n; i++){
+            //memo[i + 1] is for avoiding setting up memo[0]
             if(s.charAt(i) == 'a'){
+                //1. Delete all previous b's
+                //2. Delete current a
                 memo[i + 1] = Math.min(memo[i]+1, b);
             }else{
                 b++;
                 memo[i + 1] = memo[i];
             }
         }
-        
-        
         
         return memo[n];
     }
