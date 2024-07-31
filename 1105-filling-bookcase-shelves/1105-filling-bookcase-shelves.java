dd81b266caba891693c1521cf5ae59119062c932
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-08-01 02:45:14 +1000

    Time: 1 ms (76.60%), Space: 42.3 MB (46.20%) - LeetHub

--- 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.java
+++ 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.java
@@ -12,6 +12,10 @@ class Solution {
         for(int i = 0; i < n; i++){
             int prevWidth = books[i][0];
             int currHeight = books[i][1];
+            
+            // dp[i + 1]: books[0..i]的height sum
+            // by default, put books[i] on a new shelf
+            // - dp[i + 1] = books[0...i] height sum == books[0..i-1] height sum + books[i] height as on a new shelf
             dp[i + 1] = dp[i] + currHeight;
             int prevMaxHeight = currHeight;
             int j = i - 1;
@@ -22,8 +26,11 @@ class Solution {
                 if(prevWidth > shelfWidth){
                     break;
                 }else{
+                    
                     prevMaxHeight = Math.max(prevMaxHeight, books[j][1]);
                     
+                    // dp[i + 1] == books[0...i] height sum
+                    // == prevMaxHeight + dp[j] == books[j..i] max height + books[0..j-1] height sum
                     dp[i + 1] = Math.min(dp[i + 1], prevMaxHeight + dp[j]);
                     j--;
                 }
