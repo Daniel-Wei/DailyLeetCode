64d79acb88f27f282f403c6e9d50fb8741edf033
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2024-08-14 00:49:05 +1000

    Time: 3 ms (82.44%), Space: 43 MB (85.22%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> list = new LinkedList<List<Integer>>();
+        Arrays.sort(candidates);
+        backtrack(list, new ArrayList<Integer>(), candidates, target, 0);
+        return list;
+    }
+
+    private void backtrack(List<List<Integer>> answer, List<Integer> tempList, int[] candidates, int totalLeft, int index) 
+    {
+        if (totalLeft == 0) { // Add to the answer array, if the sum is equal to target.
+            answer.add(new ArrayList<>(tempList));
+        } 
+        else {
+            for (int i = index; i < candidates.length && totalLeft >= candidates[i]; i++) {
+                
+                //1. tempList initial start will always be unique
+                //2. when in the middle, the next start will always be unique as well
+                // - for[1, 2, 2, 2, 5] if index == 1, the next possible index is 4, so won't have multiple temp lists as [1, 2]
+                if (i > index && candidates[i] == candidates[i - 1]){
+                    continue;
+                }
+                
+                // Add it to tempList.
+                tempList.add(candidates[i]);
+                
+                // Check for all possible scenarios.
+                backtrack(answer, tempList, candidates, totalLeft - candidates[i], i + 1);
+                
+                // Backtrack the tempList.
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+}
